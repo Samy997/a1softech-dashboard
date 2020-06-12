902725bb9e6861b2363895c6ea00b16d7ac5f9e3
Samy997 <zamy.11.4.1997@gmail.com>
2020-06-12 21:17:19 +0200

    base structure done

---
+++ src/app/views/login/login-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginModule } from './login.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginModule,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LoginRoutingModule {}
